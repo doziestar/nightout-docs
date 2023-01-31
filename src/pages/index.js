30fabb024df0fbf291cbc59c73a4ecaec117b1dc
Chidozie C. Okafor <chidosiky2015@gmail.com>
2023-01-31 14:45:57 +0100

    fixing 404 error

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} Documentation`}
+      description="Documentation for Nightout <Head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
