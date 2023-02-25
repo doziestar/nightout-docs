79df0b3355711bb071793bc74176d7083ef3cdfd
Chidozie C. Okafor <chidosiky2015@gmail.com>
2023-02-25 17:41:41 +0100

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          remarkPlugins: [mdxMermaid],
+          // remarkPlugins: [mdxMermaid],
         },
         blog: {
           showReadingTime: true,
